d6d9bf18910e6ff04426bb0e44f3223c3fa25cc1
Rajesh <rajeshkhuntia00@gmail.com>
2025-05-05 22:36:07 +0530

    Initial Commit

---
+++ Muses/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Muses
+//
+//  Created by Rajesh Khuntia on 05/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Muses/MusesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusesApp.swift
+//  Muses
+//
+//  Created by Rajesh Khuntia on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MusesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
